ec6453cd95ffae3ff80cc0c9d550b5ad8b4161d7
Cameron Oliver <cameron.oliver@gmail.com>
2021-02-26 20:39:28 +1300

    Batch convert all files at once

--- code.py
+++ code.py
@@ -1,44 +1,207 @@
 import numpy as np
 import rasterio
+import re
+import os
+from pathlib import Path
+import csv
 
-# Convert tiff with 1 channel of 32-bit floats to a file with 3 channels of
-# 8-bit integers
-with rasterio.open('hirds_rainfalldepth_duration24.0_ARI100.0.tif') as src:
-    floats = src.read(1)
-    floats[floats==src.nodata]=0 # replace 'nodata' value with zero, as all
-        # depths will be greater than zero
-    print("Max",floats.max())
-    print("Min",np.min(floats[np.nonzero(floats)]))
+input_files = [
+    'hirds_rainfalldepth_duration0.5_ARI1.58.tif',
+    'hirds_rainfalldepth_duration0.5_ARI2.0.tif',
+    'hirds_rainfalldepth_duration0.5_ARI5.0.tif',
+    'hirds_rainfalldepth_duration0.5_ARI10.0.tif',
+    'hirds_rainfalldepth_duration0.5_ARI20.0.tif',
+    'hirds_rainfalldepth_duration0.5_ARI30.0.tif',
+    'hirds_rainfalldepth_duration0.5_ARI40.0.tif',
+    'hirds_rainfalldepth_duration0.5_ARI50.0.tif',
+    'hirds_rainfalldepth_duration0.5_ARI60.0.tif',
+    'hirds_rainfalldepth_duration0.5_ARI80.0.tif',
+    'hirds_rainfalldepth_duration0.5_ARI100.0.tif',
+    'hirds_rainfalldepth_duration0.5_ARI250.0.tif',
+    'hirds_rainfalldepth_duration0.166667_ARI1.58.tif',
+    'hirds_rainfalldepth_duration0.166667_ARI2.0.tif',
+    'hirds_rainfalldepth_duration0.166667_ARI5.0.tif',
+    'hirds_rainfalldepth_duration0.166667_ARI10.0.tif',
+    'hirds_rainfalldepth_duration0.166667_ARI20.0.tif',
+    'hirds_rainfalldepth_duration0.166667_ARI30.0.tif',
+    'hirds_rainfalldepth_duration0.166667_ARI40.0.tif',
+    'hirds_rainfalldepth_duration0.166667_ARI50.0.tif',
+    'hirds_rainfalldepth_duration0.166667_ARI60.0.tif',
+    'hirds_rainfalldepth_duration0.166667_ARI80.0.tif',
+    'hirds_rainfalldepth_duration0.166667_ARI100.0.tif',
+    'hirds_rainfalldepth_duration0.166667_ARI250.0.tif',
+    'hirds_rainfalldepth_duration0.333333_ARI1.58.tif',
+    'hirds_rainfalldepth_duration0.333333_ARI2.0.tif',
+    'hirds_rainfalldepth_duration0.333333_ARI5.0.tif',
+    'hirds_rainfalldepth_duration0.333333_ARI10.0.tif',
+    'hirds_rainfalldepth_duration0.333333_ARI20.0.tif',
+    'hirds_rainfalldepth_duration0.333333_ARI30.0.tif',
+    'hirds_rainfalldepth_duration0.333333_ARI40.0.tif',
+    'hirds_rainfalldepth_duration0.333333_ARI50.0.tif',
+    'hirds_rainfalldepth_duration0.333333_ARI60.0.tif',
+    'hirds_rainfalldepth_duration0.333333_ARI80.0.tif',
+    'hirds_rainfalldepth_duration0.333333_ARI100.0.tif',
+    'hirds_rainfalldepth_duration0.333333_ARI250.0.tif',
+    'hirds_rainfalldepth_duration1.0_ARI1.58.tif',
+    'hirds_rainfalldepth_duration1.0_ARI2.0.tif',
+    'hirds_rainfalldepth_duration1.0_ARI5.0.tif',
+    'hirds_rainfalldepth_duration1.0_ARI10.0.tif',
+    'hirds_rainfalldepth_duration1.0_ARI20.0.tif',
+    'hirds_rainfalldepth_duration1.0_ARI30.0.tif',
+    'hirds_rainfalldepth_duration1.0_ARI40.0.tif',
+    'hirds_rainfalldepth_duration1.0_ARI50.0.tif',
+    'hirds_rainfalldepth_duration1.0_ARI60.0.tif',
+    'hirds_rainfalldepth_duration1.0_ARI80.0.tif',
+    'hirds_rainfalldepth_duration1.0_ARI100.0.tif',
+    'hirds_rainfalldepth_duration1.0_ARI250.0.tif',
+    'hirds_rainfalldepth_duration2.0_ARI1.58.tif',
+    'hirds_rainfalldepth_duration2.0_ARI2.0.tif',
+    'hirds_rainfalldepth_duration2.0_ARI5.0.tif',
+    'hirds_rainfalldepth_duration2.0_ARI10.0.tif',
+    'hirds_rainfalldepth_duration2.0_ARI20.0.tif',
+    'hirds_rainfalldepth_duration2.0_ARI30.0.tif',
+    'hirds_rainfalldepth_duration2.0_ARI40.0.tif',
+    'hirds_rainfalldepth_duration2.0_ARI50.0.tif',
+    'hirds_rainfalldepth_duration2.0_ARI60.0.tif',
+    'hirds_rainfalldepth_duration2.0_ARI80.0.tif',
+    'hirds_rainfalldepth_duration2.0_ARI100.0.tif',
+    'hirds_rainfalldepth_duration2.0_ARI250.0.tif',
+    'hirds_rainfalldepth_duration6.0_ARI1.58.tif',
+    'hirds_rainfalldepth_duration6.0_ARI2.0.tif',
+    'hirds_rainfalldepth_duration6.0_ARI5.0.tif',
+    'hirds_rainfalldepth_duration6.0_ARI10.0.tif',
+    'hirds_rainfalldepth_duration6.0_ARI20.0.tif',
+    'hirds_rainfalldepth_duration6.0_ARI30.0.tif',
+    'hirds_rainfalldepth_duration6.0_ARI40.0.tif',
+    'hirds_rainfalldepth_duration6.0_ARI50.0.tif',
+    'hirds_rainfalldepth_duration6.0_ARI60.0.tif',
+    'hirds_rainfalldepth_duration6.0_ARI80.0.tif',
+    'hirds_rainfalldepth_duration6.0_ARI100.0.tif',
+    'hirds_rainfalldepth_duration6.0_ARI250.0.tif',
+    'hirds_rainfalldepth_duration12.0_ARI1.58.tif',
+    'hirds_rainfalldepth_duration12.0_ARI2.0.tif',
+    'hirds_rainfalldepth_duration12.0_ARI5.0.tif',
+    'hirds_rainfalldepth_duration12.0_ARI10.0.tif',
+    'hirds_rainfalldepth_duration12.0_ARI20.0.tif',
+    'hirds_rainfalldepth_duration12.0_ARI30.0.tif',
+    'hirds_rainfalldepth_duration12.0_ARI40.0.tif',
+    'hirds_rainfalldepth_duration12.0_ARI50.0.tif',
+    'hirds_rainfalldepth_duration12.0_ARI60.0.tif',
+    'hirds_rainfalldepth_duration12.0_ARI80.0.tif',
+    'hirds_rainfalldepth_duration12.0_ARI100.0.tif',
+    'hirds_rainfalldepth_duration12.0_ARI250.0.tif',
+    'hirds_rainfalldepth_duration24.0_ARI1.58.tif',
+    'hirds_rainfalldepth_duration24.0_ARI2.0.tif',
+    'hirds_rainfalldepth_duration24.0_ARI5.0.tif',
+    'hirds_rainfalldepth_duration24.0_ARI10.0.tif',
+    'hirds_rainfalldepth_duration24.0_ARI20.0.tif',
+    'hirds_rainfalldepth_duration24.0_ARI30.0.tif',
+    'hirds_rainfalldepth_duration24.0_ARI40.0.tif',
+    'hirds_rainfalldepth_duration24.0_ARI50.0.tif',
+    'hirds_rainfalldepth_duration24.0_ARI60.0.tif',
+    'hirds_rainfalldepth_duration24.0_ARI80.0.tif',
+    'hirds_rainfalldepth_duration24.0_ARI100.0.tif',
+    'hirds_rainfalldepth_duration24.0_ARI250.0.tif',
+    'hirds_rainfalldepth_duration48.0_ARI1.58.tif',
+    'hirds_rainfalldepth_duration48.0_ARI2.0.tif',
+    'hirds_rainfalldepth_duration48.0_ARI5.0.tif',
+    'hirds_rainfalldepth_duration48.0_ARI10.0.tif',
+    'hirds_rainfalldepth_duration48.0_ARI20.0.tif',
+    'hirds_rainfalldepth_duration48.0_ARI30.0.tif',
+    'hirds_rainfalldepth_duration48.0_ARI40.0.tif',
+    'hirds_rainfalldepth_duration48.0_ARI50.0.tif',
+    'hirds_rainfalldepth_duration48.0_ARI60.0.tif',
+    'hirds_rainfalldepth_duration48.0_ARI80.0.tif',
+    'hirds_rainfalldepth_duration48.0_ARI100.0.tif',
+    'hirds_rainfalldepth_duration48.0_ARI250.0.tif',
+    'hirds_rainfalldepth_duration72.0_ARI1.58.tif',
+    'hirds_rainfalldepth_duration72.0_ARI2.0.tif',
+    'hirds_rainfalldepth_duration72.0_ARI5.0.tif',
+    'hirds_rainfalldepth_duration72.0_ARI10.0.tif',
+    'hirds_rainfalldepth_duration72.0_ARI20.0.tif',
+    'hirds_rainfalldepth_duration72.0_ARI30.0.tif',
+    'hirds_rainfalldepth_duration72.0_ARI40.0.tif',
+    'hirds_rainfalldepth_duration72.0_ARI50.0.tif',
+    'hirds_rainfalldepth_duration72.0_ARI60.0.tif',
+    'hirds_rainfalldepth_duration72.0_ARI80.0.tif',
+    'hirds_rainfalldepth_duration72.0_ARI100.0.tif',
+    'hirds_rainfalldepth_duration72.0_ARI250.0.tif',
+    'hirds_rainfalldepth_duration96.0_ARI1.58.tif',
+    'hirds_rainfalldepth_duration96.0_ARI2.0.tif',
+    'hirds_rainfalldepth_duration96.0_ARI5.0.tif',
+    'hirds_rainfalldepth_duration96.0_ARI10.0.tif',
+    'hirds_rainfalldepth_duration96.0_ARI20.0.tif',
+    'hirds_rainfalldepth_duration96.0_ARI30.0.tif',
+    'hirds_rainfalldepth_duration96.0_ARI40.0.tif',
+    'hirds_rainfalldepth_duration96.0_ARI50.0.tif',
+    'hirds_rainfalldepth_duration96.0_ARI60.0.tif',
+    'hirds_rainfalldepth_duration96.0_ARI80.0.tif',
+    'hirds_rainfalldepth_duration96.0_ARI100.0.tif',
+    'hirds_rainfalldepth_duration96.0_ARI250.0.tif',
+    'hirds_rainfalldepth_duration120.0_ARI1.58.tif',
+    'hirds_rainfalldepth_duration120.0_ARI2.0.tif',
+    'hirds_rainfalldepth_duration120.0_ARI5.0.tif',
+    'hirds_rainfalldepth_duration120.0_ARI10.0.tif',
+    'hirds_rainfalldepth_duration120.0_ARI20.0.tif',
+    'hirds_rainfalldepth_duration120.0_ARI30.0.tif',
+    'hirds_rainfalldepth_duration120.0_ARI40.0.tif',
+    'hirds_rainfalldepth_duration120.0_ARI50.0.tif',
+    'hirds_rainfalldepth_duration120.0_ARI60.0.tif',
+    'hirds_rainfalldepth_duration120.0_ARI80.0.tif',
+    'hirds_rainfalldepth_duration120.0_ARI100.0.tif',
+    'hirds_rainfalldepth_duration120.0_ARI250.0.tif',
+]
 
-    #Taken from https://gis.stackexchange.com/a/272805
-    zeros = np.zeros(floats.shape)
-    mask = floats != 0
-    r = np.where(mask, np.floor_divide((100000 + floats * 10), 65536), zeros)
-    g = np.where(mask, np.floor_divide((100000 + floats * 10), 256) - r * 256, zeros)
-    b = np.where(mask, np.floor(100000 + floats * 10) - r * 65536 - g * 256, zeros)
+filename_regex = re.compile(
+    r'hirds_rainfalldepth_duration(?P<duration>\d+.\d+)_ARI(?P<ari>\d+.\d+).tif')
 
-    """
-    treated_as_ints = np.zeros((3, floats.shape[1], floats.shape[2]))
-    for channel in floats:
-        for i, row in enumerate(channel):
-            for j, cell in enumerate(row):
-                if cell == 0:
-                    v = 0
-                else:
-                    v = int(round(cell*100,0))
-                    v += 10000
-                a,b,c = np.frombuffer(
-                    v.to_bytes(3, byteorder="big", signed=False),
-                    dtype=np.uint8)
-                treated_as_ints[0][i][j] = a
-                treated_as_ints[1][i][j] = b
-                treated_as_ints[2][i][j] = c """
 
-    # Write to a new 3-channel 8-bit file.
-    profile = src.profile
-    profile.update(dtype=rasterio.uint8, count=3, compress='lzw', nodata=0)
-    with rasterio.open('example.tif', 'w', **profile) as dst:
-        #dst.write(treated_as_ints.astype(rasterio.uint8))
-        dst.write_band(1, r.astype(rasterio.uint8))
-        dst.write_band(2, g.astype(rasterio.uint8))
-        dst.write_band(3, b.astype(rasterio.uint8))
+def convert(filename, output_folder):
+    # Convert tiff with 1 channel of 32-bit floats to a file with 3 channels of
+    # 8-bit integers
+    try:
+        with rasterio.open(os.path.join('Original data',filename)) as src:
+            floats = src.read(1)
+            floats[floats==src.nodata]=0 # replace 'nodata' value with zero, as all
+                # depths will be greater than zero
+            max = floats.max()
+            min = np.min(floats[np.nonzero(floats)])
+
+            #Taken from https://gis.stackexchange.com/a/272805
+            zeros = np.zeros(floats.shape)
+            mask = floats != 0
+            r = np.where(mask, np.floor_divide((100000 + floats * 10), 65536), zeros)
+            g = np.where(mask, np.floor_divide((100000 + floats * 10), 256) - r * 256, zeros)
+            b = np.where(mask, np.floor(100000 + floats * 10) - r * 65536 - g * 256, zeros)
+
+            # Write to a new 3-channel 8-bit file.
+            profile = src.profile
+            profile.update(dtype=rasterio.uint8, count=3, compress='lzw', nodata=0)
+            output_file = os.path.join(output_folder, 'dem_source_data.tif')
+            with rasterio.open(output_file, 'w', **profile) as dst:
+                #dst.write(treated_as_ints.astype(rasterio.uint8))
+                dst.write_band(1, r.astype(rasterio.uint8))
+                dst.write_band(2, g.astype(rasterio.uint8))
+                dst.write_band(3, b.astype(rasterio.uint8))
+            os.system("/usr/local/Cellar/python@3.9/3.9.1_8/bin/python3 "+\
+                "/usr/local/bin/gdal2tiles.py --xyz -w 'leaflet' -z '5' "+\
+                "-s 'EPSG:27200' '"+output_file+"' '"+output_folder+"'")
+        return min, max
+    except:
+        print("Could not open",filename)
+        return None, None
+
+
+with open('min_max.csv', 'w', newline='') as minmax_file:
+    minmax_writer = csv.writer(minmax_file)
+    minmax_writer.writerow(['ARI (years)','Duration (hours)', 'Min depth (mm)' 'Max depth (mm)'])
+
+    for filename in input_files:
+        print(filename)
+        d = filename_regex.match(filename).groupdict()
+        ari_folder = d['ari']+'yr'
+        duration_folder = d['duration']+'hr'
+        combined_folder = os.path.join('tiles',ari_folder,duration_folder)
+        Path(combined_folder).mkdir(parents=True, exist_ok=True)
+        min, max = convert(filename, combined_folder)
+        minmax_writer.writerow([d['ari'],d['duration'],min,max])        
